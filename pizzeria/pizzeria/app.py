94971abbd940c12198ee4c88a91f2a97f0f5e106
Bianca <bpas@cin.ufpe.br>
2026-01-11 20:13:47 -0300

    test: verifica se a rota de check health retorna um json com status ok

--- pizzeria/pizzeria/app.py
+++ pizzeria/pizzeria/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
 from pizzeria.schemas import HealthCkeck
-from http import HTTPStatus
 
 app = FastAPI(title='Pizzeria API')
 
+
 @app.get('/api/status', response_model=HealthCkeck)
 def get_api_status():
-    return HealthCkeck(status="OK")
+    return HealthCkeck(status='OK')
--- pizzeria/pizzeria/schemas.py
+++ pizzeria/pizzeria/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
 
+
 class HealthCkeck(BaseModel):
-    status: str = "OK"
+    status: str = 'OK'
